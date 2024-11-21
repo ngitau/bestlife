8a2498b70b4b28096037b87692726e1174a482dc
Joseph G. <joseph@ndungugitau.com>
2024-11-21 12:19:57 +0300

    Move key validations to `CustomAttribute` model

--- app/models/concerns/attributable.rb
+++ app/models/concerns/attributable.rb
@@ -7,10 +7,9 @@ module Attributable
 
   def set_custom_attribute(key:, value:)
     custom_attribute = custom_attributes.find_or_initialize_by(key:)
-    validate_key!(custom_attribute:, key:)
-    return custom_attribute if custom_attribute.errors.any?
-
     custom_attribute.value = value
+    return custom_attribute unless custom_attribute.valid?
+
     custom_attribute.save!
   end
 
@@ -18,22 +17,5 @@ module Attributable
     return nil if key.blank?
 
     custom_attributes.find_by(key:).try(:value)
-  end
-
-  private
-
-  def custom_fields
-    associated_model = self.class.name.underscore
-    CustomField.by_model(associated_model:)
-  end
-
-  def validate_key!(custom_attribute:, key:)
-    if key.blank?
-      custom_attribute.errors.add(:key, :blank)
-    elsif custom_fields.blank?
-      custom_attribute.errors.add(:key, :custom_fields_not_set)
-    elsif !key.in?(custom_fields)
-      custom_attribute.errors.add(:key, :invalid_key)
-    end
   end
 end
--- spec/models/custom_attribute_spec.rb
+++ spec/models/custom_attribute_spec.rb
@@ -5,7 +5,11 @@ describe CustomAttribute do
   it { should validate_presence_of(:value) }
 
   context 'when key is provided' do
-    subject { create(:custom_attribute_for_customer) }
+    subject { create(:custom_attribute_for_customer, key:) }
+
+    let(:key) { Faker::Lorem.word }
+
+    before { create(:custom_field, name: key, associated_model: 'customer') }
 
     it { should validate_uniqueness_of(:key).scoped_to(:attributable_type, :attributable_id).ignoring_case_sensitivity }
   end
